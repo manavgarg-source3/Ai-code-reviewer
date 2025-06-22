caef573a179a318ea4b9325660e42474f3a1a4ff
“manavgarg-source3” <manavku150@gmail.com>
2025-06-22 15:30:50 +0530

    Updated frontend to use deployed backend API

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors(
     {
         origin: 'http://localhost:5173',
+        origin: 'https://ai-code-reviewer-beryl-six.vercel.app/',
+
         credentials: true
         }   
 ))
